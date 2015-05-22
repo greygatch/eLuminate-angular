94446b975eb8ac4a1201ede3ffa231ad8a47a5ed
Evan Gatchell <greygatch@gmail.com>
2015-05-22 04:20:44 -0700

    it begins

--- client/config/config.js
+++ client/config/config.js
@@ -6,9 +6,9 @@ angular.module('poseidon')
 
   $stateProvider
   .state('home', {url: '/', templateUrl: '/views/general/home.html'})
-  .state('about', {url: '/about', templateUrl: '/views/general/about.html'})
-  .state('faq', {url: '/faq', templateUrl: '/views/general/faq.html'})
-  .state('contact', {url: '/contact', templateUrl: '/views/general/contact.html'})
+  .state('news', {url: '/news', templateUrl: '/views/general/news.html'})
+  .state('forum', {url: '/forum', templateUrl: '/views/general/forum.html'})
+  .state('profile', {url: '/profile', templateUrl: '/views/general/profile.html'})
   .state('register', {url: '/register', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
   .state('login', {url: '/login', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'});
 });
