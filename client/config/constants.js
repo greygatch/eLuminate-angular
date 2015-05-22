fee7aa63b6070002472b25a2cad4c20ca85da5e9
Evan Gatchell <greygatch@gmail.com>
2015-05-21 18:47:56 -0700

    initial, fixed sweet alert path

--- client/config/constants.js
+++ client/config/constants.js
@@ -1,5 +1,5 @@
 'use strict';
 
 angular.module('poseidon')
-.constant('firebaseUrl', 'https://__________.firebaseio.com/')
+.constant('firebaseUrl', 'https://login-greygatch.firebaseio.com/')
 .constant('nodeUrl', 'http://localhost:8000');
