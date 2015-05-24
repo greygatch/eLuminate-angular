ea4cbf5e46a3647f78f9a032cadc589f78c2d4e8
Evan Gatchell <greygatch@gmail.com>
2015-05-24 15:29:47 -0700

    carousel

--- client/views/newsmap/news.js
+++ client/views/newsmap/news.js
@@ -2,7 +2,10 @@
 
 angular.module('poseidon')
 .controller('NewsCtrl', function($rootScope, $scope, $state, $window, $firebaseObject, $http, Map, User){
+
+  $scope.i = 0;
   $scope.isRead = true;
+
   Map.geocode('london', function(result){
     Map.create('#mapDiv', 25, 0, 2);
     $window.jQuery.getJSON('http://api.nytimes.com/svc/topstories/v1/home.json?api-key=d9aaa709a92d3d4ad4efbd1902a469ac:19:72139126', function(response){
@@ -14,9 +17,9 @@ angular.module('poseidon')
       articles.forEach(function(e){
         locations.push(e.geo_facet[0]);
       });
-      // console.log('locations', locations)
       $scope.$apply(function(){
         $scope.articles = articles;
+        $scope.article = $scope.articles[0];
       });
     })
   });
@@ -24,9 +27,15 @@ angular.module('poseidon')
   $scope.isRead = function(){
     var readingPoints = Math.floor(Math.random() * 25) + 75;
     console.log(readingPoints);
-    debugger;
-
   }
-});
 
-// article.multimedia[multimedia.length-1].url
+  $scope.move = function(direction){
+    if(direction === 'next' && $scope.i < $scope.articles.length - 1){
+     $scope.i += 1;
+    }else if(direction === 'prev' && $scope.i > 0){
+     $scope.i -= 1;
+    }
+    $scope.article = $scope.articles[$scope.i];
+  }
+
+});
