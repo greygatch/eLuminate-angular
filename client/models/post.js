f51fb59fb9015a1e784b9fd132ececc09792478b
Evan Gatchell <greygatch@gmail.com>
2015-05-26 14:36:09 -0700

    das forum

--- client/models/post.js
+++ client/models/post.js
@@ -5,8 +5,11 @@ angular.module('poseidon')
   function Post(){
   }
 
+  Post.find = function(){
+    return $http.get(nodeUrl + '/posts');
+  }
   Post.create = function(post){
-    return $http.post(nodeUrl + '/posts', post)
+    return $http.post(nodeUrl + '/posts', post);
   }
 
   return Post;
--- client/views/forum/forum.js
+++ client/views/forum/forum.js
@@ -2,11 +2,20 @@
 
 angular.module('poseidon')
 .controller('ForumCtrl', function($rootScope, $scope, $state, $firebaseObject, $http, User, Post){
+  Post.find()
+  .then(function(response){
+    console.log('POSTS', response);
+    $scope.posts = response.data;
+  });
+
   $scope.createPost = function(post){
     post.id = $rootScope.activeUser.uid;
     Post.create(post)
     .then(function(response){
-      console.log(response);
+      Post.find()
+      .then(function(response){
+        $scope.posts = response.data;
+      });
     })
   }
 });
