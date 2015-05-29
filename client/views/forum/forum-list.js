fb35a4e0ea96823d58585ab07ae3d9dc80520bbc
Evan Gatchell <greygatch@gmail.com>
2015-05-29 02:15:17 -0700

    edit and delete buttons on show page

--- client/views/forum/forum-list.js
+++ client/views/forum/forum-list.js
@@ -2,11 +2,25 @@
 
 angular.module('poseidon')
 .controller('ForumListCtrl', function($rootScope, $scope, $state, $firebaseObject, $http, User, Post){
+  var UID;
+
   Post.find()
   .then(function(response){
     $scope.posts = response.data;
     console.log($scope.posts);
   });
+
+  User.find()
+  .then(function(response){
+    $scope.user = response.data;
+    UID = response.data._id;
+    console.log('UID', UID);
+  });
+
+  $scope.isUserPost = function(post){
+    return post.userId === UID;
+  }
+
   $scope.upvote = function(post){
     post.votes += 1;
     Post.edit(post)
