87333484da82e0e067583001f77d890230c9223b
Evan Gatchell <greygatch@gmail.com>
2015-05-28 19:35:00 -0700

    bells and whistles

--- client/views/newsmap/news.js
+++ client/views/newsmap/news.js
@@ -25,7 +25,7 @@ angular.module('poseidon')
     user = response.data;
   });
 
-  Map.geocode('tehran', function(){
+  Map.geocode('rome', function(){
     var map = Map.create('#mapDiv', 25, 0, 2);
     $window.jQuery.getJSON('http://api.nytimes.com/svc/topstories/v1/home.json?api-key=d9aaa709a92d3d4ad4efbd1902a469ac:19:72139126', function(response){
       var articles = response.results.filter(function(e){
